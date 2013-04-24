7322b0059fbd29d840b7b3b9cf0a0c8f07cca03b
François Poirotte <clicky@erebot.net>
2013-04-24 21:46:44 +0200

    Add dependency on MySQLdb-python.

--- setup.py
+++ setup.py
@@ -51,6 +51,7 @@ setup(
 #        "TgRum",
         "Babel >=0.9.4",
         "tg.devtools", # Required during bootstrap.
+        "MySQL-python",
     ],
     setup_requires=["PasteScript >= 1.7"],
     paster_plugins=['PasteScript', 'Pylons', 'TurboGears2', 'tg.devtools'],
